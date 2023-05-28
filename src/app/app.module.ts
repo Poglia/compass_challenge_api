6af812cbfc031d4e16f2de9e84ca232adfe5d801
Poglia <pedro.poglia@hotmail.com>
2023-05-28 20:40:39 -0300

    feat: create connection db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from './post/post.entity';
+import { UserEntity } from './user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://0.0.0.0:27017',
+      database: 'compass',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    TypeOrmModule.forFeature([PostEntity, UserEntity]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
