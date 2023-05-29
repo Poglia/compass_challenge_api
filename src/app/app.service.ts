409d7365a20189b6e85f28b4fdaa9f0c3a657ab9
Poglia <pedro.poglia@hotmail.com>
2023-05-29 09:51:33 -0300

    refactor: chage message app service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Vamos iniciar essa bagaça";
+    return "App Service";
   }
 }
