956bf27f49076e84caed4d9cd69b253be4a32672
Poglia <pedro.poglia@hotmail.com>
2023-05-29 14:27:50 -0300

    feat: validations create-user-dto

--- src/app/user/dto/create.dto.ts
+++ src/app/user/dto/create.dto.ts
@@ -1,15 +1,29 @@
-export class CreateUserDto {
-  user_id: number;
+import { User } from "../entity/user.entity";
+import {
+  IsString,
+  IsEmail,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
 
+export class CreateUserDto {
+
+  @IsString()
   name: string;
 
+  @IsString()
   user: string;
 
+  @IsString()
   birthdate: string;
 
+  @IsEmail()
   email: string;
 
+  @IsString()
+  @MinLength(6)
   password: string;
 
+  @IsOptional()
   profile_photo: string;
 }
