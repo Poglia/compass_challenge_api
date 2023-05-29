4837a6b172b959860bccf37f8e046ea5f19fafa2
Poglia <pedro.poglia@hotmail.com>
2023-05-29 09:26:21 -0300

    perf: exclude mongodb of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostEntity } from "./post/post.entity";
 import { UserEntity } from "./user/user.entity";
 import { UserController } from "./user/user.controller";
@@ -9,16 +8,6 @@ import { UserController } from "./user/user.controller";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mongodb",
-      url: "mongodb://0.0.0.0:27017",
-      database: "compass",
-      entities: [__dirname + "/**/*.entity{.ts,.js}"],
-      ssl: false,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }),
-    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AppController, UserController],
   providers: [AppService],
--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -7,36 +7,29 @@ import {
   Delete,
   Body,
   Param,
-  NotFoundException,
 } from "@nestjs/common";
 import { UserEntity } from "../user/user.entity";
-import { MongoRepository } from "typeorm/repository/MongoRepository";
-import { InjectRepository } from "@nestjs/typeorm";
 
 @Controller("api/v1/users")
 export class UserController {
-  constructor(
-    @InjectRepository(UserEntity)
-    private readonly usersRepository: MongoRepository<UserEntity>,
-  ) {}
 
-  @Get()
-  public async getUsers(): Promise<UserEntity[]> {
-    return await this.usersRepository.find();
-  }
+  // @Get()
+  // public async getUsers(): Promise<UserEntity[]> {
+  //   return await this.usersRepository.find();
+  // }
 
-  @Get(":user_id")
-  public async getUser(@Param() params): Promise<UserEntity[]> {
-    return await this.usersRepository.find({ user_id: Number(params.user_id) });
-  }
+  // @Get(":user_id")
+  // public async getUser(@Param() params): Promise<UserEntity[]> {
+  //   return await this.usersRepository.find({ user_id: Number(params.user_id) });
+  // }
 
-  @Post()
-  public async createUser(
-    @Body() user: Partial<UserEntity>,
-  ): Promise<UserEntity> {
-    return await this.usersRepository.save(new UserEntity(user));
-    // return new UserEntity(user)
-  }
+  // @Post()
+  // public async createUser(
+  //   @Body() user: Partial<UserEntity>,
+  // ): Promise<UserEntity> {
+  //   return await this.usersRepository.save(new UserEntity(user));
+  //   // return new UserEntity(user)
+  // }
 
   @Put(":id")
   async update(@Body() body, @Param() params) {
